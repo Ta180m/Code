fdedb3f81c4861d1382034efd59ba7e24bd563a7
Anthony Wang <ta180m@pm.me>
2021-07-06 10:44:27 -0500

    Remove input redirection

--- 2.cpp
+++ 2.cpp
@@ -7,7 +7,6 @@ using ii = pair<int, int>;
 constexpr int MX = 1e5+5;
 
 int main() {
-	if (fopen("in", "r")) freopen("in", "r", stdin), freopen("out", "w", stdout);
 	ios_base::sync_with_stdio(0), cin.tie(0);
 
 
